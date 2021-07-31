f5836e863d8871d147c2b077e2cea9817ece0433
Ahmad K. Abdullah <ahmadkabdullah@pm.me>
2021-08-01 00:52:39 +0300

    Refactor code while staying with original procedual style

--- main.go
+++ main.go
@@ -12,8 +12,7 @@ import (
 	"strings"
 )
 
-//error and recovery functions
-func recovery() {
+func printErrorAndExit() {
 	if r := recover(); r != nil {
 		fmt.Println(r)
 		os.Exit(1)
@@ -21,37 +20,37 @@ func recovery() {
 }
 
 func main() {
-	//number of args
-	argsNum := uint8(len(os.Args))
+	//var for number of arguments
+	argsNum := len((os.Args)) - 1
 
 	//if no args given
 	switch argsNum {
-	case 1:
+	case 0:
 		fmt.Println("EpubGoTerm: \n\tGo utility to print EPUB chapters into the terminal.")
-		fmt.Println("Syntax:\n\tepubgot epubfile chapternumber\t# print a chapter in the EPUB\n\tepubgot epubfile\t\t# list chapters of the EPUB and print image count")
+		fmt.Println("Use:\n\tepubgot epubfile chapternumber\t# print a chapter in the EPUB\n\tepubgot epubfile\t\t# list chapters of the EPUB and print image count")
 		os.Exit(0)
 	}
 
 	//recovery function
-	defer recovery()
+	defer printErrorAndExit()
 
-	//make arg1
-	var arg1 = os.Args[1]
-	//check if epub and open
-	if filepath.Ext(arg1) != ".epub" {
-		panic(fmt.Errorf("(1) file is not an epub: %v", arg1))
+	//check if epub
+	if filepath.Ext(os.Args[1]) != ".epub" {
+		panic(fmt.Errorf("(1) file is not an epub: %v", os.Args[1]))
 	}
-	archv, err := zip.OpenReader(arg1)
+
+	//open epub
+	archv, err := zip.OpenReader(os.Args[1])
 	defer archv.Close()
 	if err != nil {
-		panic(fmt.Errorf("(1) failed to open epub: %v", arg1))
+		panic(fmt.Errorf("(1) failed to open epub: %v", os.Args[1]))
 	}
 
 	switch argsNum {
-	case 2:
+	//LIST when given one argument
+	case 1:
 		//make counters
-		var chpCount uint
-		var imgCount uint
+		var chpCount, imgCount uint
 
 		//case file extension and act
 		for _, fileInArchv := range archv.File {
@@ -63,10 +62,12 @@ func main() {
 				imgCount++
 			}
 		}
+
 		//print counters
 		fmt.Println("Chapters:", chpCount, "Images:", imgCount)
 
-	case 3:
+	//OPEN when given two arguments
+	case 2:
 		//make temporary directory
 		tmp, err := ioutil.TempDir(os.TempDir(), "epubgot-book*")
 		defer os.RemoveAll(tmp)
@@ -74,73 +75,82 @@ func main() {
 			panic(fmt.Errorf("(1) could not create temporary dir: %v", tmp))
 		}
 
-		//make a list and arg2
-		list := make([]string, 1, 1500)
-		num, _ := strconv.Atoi(os.Args[2])
-		x := uint16(num)
+		//make counter and arg2
+		var htmlFileCounter int
+		var foundFile bool
+		wantedChapter, _ := strconv.Atoi(os.Args[2])
 
 		//range through files and find htmls
 		for _, fileInArchive := range archv.File {
-			switch filepath.Ext(fileInArchive.Name) {
-			case ".html", ".xhtml":
-				//join path, append it to list, and get list len
-				fpath := filepath.Join(tmp, fileInArchive.Name)
-				list = append(list, fpath)
-				var listLen = uint16(len(list) - 1)
-
-				if listLen == x {
-					//check for zipslip (https://snyk.io/research/zip-slip-vulnerability)
-					if !strings.HasPrefix(fpath, filepath.Clean(tmp)+string(os.PathSeparator)) {
-						panic(fmt.Errorf("(2) detected illegal file path: %v", fpath))
-					}
-
-					//stat dir of file, make if not exists
-					_, err := os.Stat(filepath.Dir(fpath))
-					if os.IsNotExist(err) {
-						os.MkdirAll(filepath.Dir(fpath), 0777)
-					}
-
-					//open the file, make new file, and copy new to opened
-					fileOpened, err := fileInArchive.Open()
-					if err != nil {
-						panic(fmt.Errorf("(2) failed to open html file of selected chapter: %v", fileInArchive.Name))
-					}
-
-					fileCreated, err := os.Create(fpath)
-					if err != nil {
-						panic(fmt.Errorf("(2) failed to create html file at temporary dir: %v", fpath))
-					}
-
-					_, err = io.Copy(fileCreated, fileOpened)
-					if err != nil {
-						panic(fmt.Errorf("(2) failed to copy html file to temporary dir: %v to %v", fileInArchive.Name, fpath))
-					}
-
-					//close
-					fileOpened.Close()
-					fileCreated.Close()
-
-					//read selected file
-					htmlFile, err := ioutil.ReadFile(list[x])
-					if err != nil {
-						panic(fmt.Errorf("(3) could not read html file at temporary dir: %v", list[x]))
-					}
-
-					// convert to string
-					html := string(htmlFile)
-
-					txt := convertHTMLtoText(html)
-
-					//print result
-					fmt.Println("Chapter:", x)
-					fmt.Println(txt)
-				}
+			//if not html then skip
+			fileExt := filepath.Ext(fileInArchive.Name)
+			if fileExt != ".html" && fileExt != ".xhtml" {
+				continue
 			}
+
+			//add to the counter
+			htmlFileCounter++
+
+			//if not at wanted item then skip
+			if htmlFileCounter != wantedChapter {
+				continue
+			} else {
+				foundFile = true
+			}
+
+			//set path for the target file
+			targetFilePath := filepath.Join(tmp, fileInArchive.Name)
+
+			//check path for zipslip (https://snyk.io/research/zip-slip-vulnerability)
+			if !strings.HasPrefix(targetFilePath, filepath.Clean(tmp)+string(os.PathSeparator)) {
+				panic(fmt.Errorf("(2) detected illegal file path: %v", targetFilePath))
+			}
+
+			//stat dir of file, make if not exists
+			_, err := os.Stat(filepath.Dir(targetFilePath))
+			if os.IsNotExist(err) {
+				os.MkdirAll(filepath.Dir(targetFilePath), 0777)
+			}
+
+			//open the file
+			fileOpened, err := fileInArchive.Open()
+			defer fileOpened.Close()
+			if err != nil {
+				panic(fmt.Errorf("(2) failed to open html file of selected chapter: %v", fileInArchive.Name))
+			}
+
+			//make new file
+			fileCreated, err := os.Create(targetFilePath)
+			defer fileCreated.Close()
+			if err != nil {
+				panic(fmt.Errorf("(2) failed to create html file at temporary dir: %v", targetFilePath))
+			}
+
+			//copy new file to opened file
+			_, err = io.Copy(fileCreated, fileOpened)
+			if err != nil {
+				panic(fmt.Errorf("(2) failed to copy html file to temporary dir: %v to %v", fileInArchive.Name, targetFilePath))
+			}
+
+			//read selected file
+			htmlFile, err := ioutil.ReadFile(targetFilePath)
+			if err != nil {
+				panic(fmt.Errorf("(3) could not read html file at temporary dir: %v", targetFilePath))
+			}
+
+			// convert to string
+			htmlFileAsString := string(htmlFile)
+
+			convertedText := convertHTMLtoText(htmlFileAsString)
+
+			//print result
+			fmt.Println("Chapter:", wantedChapter)
+			fmt.Println(convertedText)
 		}
 
-		//if is 0 or more than list
-		if 0 == x || x > uint16(len(list)-1) {
-			panic(fmt.Errorf("(3) number is out of epub's chapter range: %v", x))
+		//imperfect range detection
+		if foundFile == false {
+			panic(fmt.Errorf("(2) chapter number given is likely out of range: %v", wantedChapter))
 		}
 	}
 }
